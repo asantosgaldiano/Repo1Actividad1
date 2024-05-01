d5eabefb3ae67aeeb8e6a12eb39b4bf19d36f572
Alberto <albertosantosgaldiano@gmail.com>
2024-05-01 17:21:55 +0200

    Soy alberto y he añadido mi clase

---
+++ src/alberto/Alberto.java
@@ -0,0 +1,13 @@
+package alberto;
+
+public class Alberto {
+
+	public static void main(String[] args) {
+		System.out.println(alberto());
+
+	}
+
+	public static String alberto() {
+		return "Hola, soy Alberto"; 
+	}
+}
